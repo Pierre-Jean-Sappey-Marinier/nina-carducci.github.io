cf689bfe4ba9b8c31a64964650002a71cc2cf17f
Pierre-Jean-Sappey-Marinier <113429394+Pierre-Jean-Sappey-Marinier@users.noreply.github.com>
2023-02-15 15:38:23 +0100

    ajout de packages

---
+++ assets/rollup.config.mjs
@@ -0,0 +1,8 @@
+export default {
+  input: "bootstrap/bootstrap.bundle.min.js",
+  output: {
+    dir: "bootstrap/dist",
+    format: "es",
+    sourceMap: "inline",
+  },
+};
---
+++ assets/rollup.config2.mjs
@@ -0,0 +1,8 @@
+export default {
+  input: "bootstrap/bootstrap.min.css",
+  output: {
+    dir: "bootstrap/dist/lalala.js",
+    format: "iife",
+    sourceMap: "inline",
+  },
+};
